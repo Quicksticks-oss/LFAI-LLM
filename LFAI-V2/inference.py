8e6d8eb14d02738a5f0e7c7b7bb9b17f1d02dc82
Quicksticks-oss <chongobongotoday@gmail.com>
2023-09-12 12:23:18 -0400

    Updated inference for v2 model

--- LFAI-V2/inference.py
+++ LFAI-V2/inference.py
@@ -2,27 +2,32 @@ import torch
 import torch.nn as nn
 from torch.nn import functional as F
 from tokenizer.tokenizer import CharBasedTokenizer
-from TRAIN_SETTINGS import *
 from model import LanguageModel
-import json
-import os
 
-device = torch.device('cpu')
-with open('tokens.json', 'r', encoding='utf-8') as f:
-    chars = json.loads(f.read())['chars']
+### PARAMS ###
+MODEL = "model.pt"
+DEVICE = 'cpu'
+### ###### ###
 
-tokenizer = CharBasedTokenizer(chars)
+if __name__ == '__main__':
+    device = torch.device(DEVICE)
 
-# Load the state_dict into the model
-model = LanguageModel(tokenizer.vocab_size, n_embd,
-                          n_layer, CONTEXT_SIZE, device)
-state_dict = torch.load("current.pt", map_location="cpu")
-model.load_state_dict(state_dict)
-model = model.to(device)
+    save_out = torch.load(MODEL, map_location="cpu")
+    n_embd = save_out["n_embd"]
+    n_layer = save_out["n_layer"]
+    CONTEXT_SIZE = save_out["ctx"]
 
-model.eval()
-# generate from the model
-context = torch.zeros((1, 1), dtype=torch.long, device=device)
-genned, hidden = model.generate(context, max_new_tokens=2000)
-genned = genned[0].tolist()
-print(tokenizer.decode(genned))
+    tokenizer = CharBasedTokenizer(chars)
+
+    # Load the state_dict into the model
+    model = LanguageModel(tokenizer.vocab_size, n_embd,
+                            n_layer, CONTEXT_SIZE, device)
+    state_dict = torch.load("current.pt", map_location="cpu")
+    model.load_state_dict(state_dict)
+    model = model.to(device)
+    model.eval()
+
+    context = torch.zeros((1, 1), dtype=torch.long, device=device)
+    genned, hidden = model.generate(context, max_new_tokens=2000)
+    genned = genned[0].tolist()
+    print(tokenizer.decode(genned))
